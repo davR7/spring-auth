3212144cc486c70440a8035ba0d81cfde3066b73
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-08 15:12:18 -0300

    created - UserAuthenticationFilter

---
+++ src/main/java/com/davr7/springauth/infra/security/UserAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.davr7.springauth.infra.security;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.davr7.springauth.domain.User;
+import com.davr7.springauth.domain.UserDetailsImpl;
+import com.davr7.springauth.repositories.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserAuthenticationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+		if (isEndpointPrivate(request)) {
+			
+			String token = recoveryToken(request);
+			
+			if (token != null) {
+				String subject = jwtUtil.verifyToken(token);
+				User user = repository.findByUsername(subject).get();
+				UserDetailsImpl userDetails = new UserDetailsImpl(user);
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			} else {
+				throw new RuntimeException("The token is missing.");
+			}
+			
+			try {
+				filterChain.doFilter(request, response);
+			} catch (IOException | ServletException e) {
+				throw new RuntimeException("Error during filter. "+e);
+			}
+		}
+	}
+	
+	private String recoveryToken(HttpServletRequest request) {
+		String AuthHeader = request.getHeader("Authorization");
+		if (AuthHeader != null && AuthHeader.startsWith("Bearer")) {
+			return AuthHeader.substring(7);
+		}
+		return null;
+	}
+	
+	private Boolean isEndpointPrivate(HttpServletRequest request) {
+		String requestURI = request.getRequestURI();
+		return !Arrays.asList(EndpointConfig.ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).contains(requestURI);
+	}
+
+}
