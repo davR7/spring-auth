1cd038c23d9c55834ce20bce6a62667a8b358f9f
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-08 16:11:28 -0300

    added - signup method to the AuthenticationResource class

--- src/main/java/com/davr7/springauth/resources/AuthenticationResource.java
+++ src/main/java/com/davr7/springauth/resources/AuthenticationResource.java
@@ -1,9 +1,16 @@
 package com.davr7.springauth.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.davr7.springauth.domain.User;
+import com.davr7.springauth.dtos.UserRegisterDTO;
 import com.davr7.springauth.services.UserService;
 
 @RestController
@@ -12,4 +19,18 @@ public class AuthenticationResource {
 	
 	@Autowired
 	UserService service;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<User> signup(UserRegisterDTO obj) {
+
+		User user = service.createUser(obj);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(user.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).body(user);
+	}
 }
