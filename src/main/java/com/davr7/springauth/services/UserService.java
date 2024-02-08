ec672eab7352a690f17f9d3b860e1d9927e6ad4c
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-08 15:48:59 -0300

    added - readAllUsers method to the UserService class

--- src/main/java/com/davr7/springauth/services/UserService.java
+++ src/main/java/com/davr7/springauth/services/UserService.java
@@ -1,8 +1,11 @@
 package com.davr7.springauth.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davr7.springauth.domain.User;
 import com.davr7.springauth.repositories.UserRepository;
 
 @Service
@@ -10,4 +13,8 @@ public class UserService {
 	
 	@Autowired
 	UserRepository repository;
+	
+	public List<User> readAllUsers(){
+		return repository.findAll();
+	}
 }
