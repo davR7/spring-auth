aff8d20d5fb8a87b7ae53302bd92411e5c4e0086
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-07 22:13:16 -0300

    created - UserDatailsImpl class

---
+++ src/main/java/com/davr7/springauth/domain/user/UserDetailsImpl.java
@@ -0,0 +1,60 @@
+package com.davr7.springauth.domain.user;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+
+@Getter
+public class UserDetailsImpl implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
+	private User user;
+	
+	public UserDetailsImpl(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return user
+				.getRoles()
+				.stream()
+				.map(role -> role.toGrantedAuthority())
+				.collect(Collectors.toList());
+	}
+	
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
