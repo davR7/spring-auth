798c8ba1cc0c516471570cf2faabdd575e49943d
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-08 19:41:12 -0300

    updated - enum with string attribute

--- src/main/java/com/davr7/springauth/domain/UserRole.java
+++ src/main/java/com/davr7/springauth/domain/UserRole.java
@@ -1,32 +1,16 @@
 package com.davr7.springauth.domain;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 public enum UserRole {
-	ADMIN(1),
-	BASIC(2);
+	ADMIN("ROLE_ADMIN"),
+	BASIC("ROLE_BASIC");
 	
-	private int userCode;
-	
-	private UserRole(int userCode) {
-		this.userCode = userCode;
-	};
+	private String role;
 
-	public int getRoleCode() {
-		return userCode;
-	};
-	
-	public GrantedAuthority toGrantedAuthority() {
-		return new SimpleGrantedAuthority("ROLE_"+ name());
-	}
-	
-	public static UserRole valueOf(int userCode) {
-		for (UserRole value : UserRole.values()) {
-			if (value.getRoleCode() == userCode) {
-				return value;
-			}
-		}
-		throw new IllegalArgumentException("Invalid UserRole Code");
-	}
+    UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
 }
