2d5e5b9578dd34ee4904d4c5eda98f19a96b354c
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-11 10:40:23 -0300

    modified - attributes ADMIN and BASIC to the ROLE_ADMIN and ROLE_BASIC

--- src/main/java/com/davr7/springauth/domain/UserRole.java
+++ src/main/java/com/davr7/springauth/domain/UserRole.java
@@ -1,16 +1,6 @@
 package com.davr7.springauth.domain;
 
 public enum UserRole {
-	ADMIN("ROLE_ADMIN"),
-	BASIC("ROLE_BASIC");
-	
-	private String role;
-
-    UserRole(String role){
-        this.role = role;
-    }
-
-    public String getRole(){
-        return role;
-    }
+	ROLE_ADMIN,
+	ROLE_BASIC
 }
