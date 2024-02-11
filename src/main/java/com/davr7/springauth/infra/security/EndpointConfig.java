13212fcb6ca18aeae70e3c6a6d46f0a9c5599da6
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-11 09:46:50 -0300

    deleted file - EndpointConfig.java

--- src/main/java/com/davr7/springauth/infra/security/EndpointConfig.java
+++
@@ -1,9 +0,0 @@
-package com.davr7.springauth.infra.security;
-
-public class EndpointConfig {
-	
-	public static final String[] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
-		"/auth/signin",
-		"/auth/signup"
-	};
-}
