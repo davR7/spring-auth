5cb910b02217902d59b7c7a9db77d264ae5e0619
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-11 11:44:08 -0300

    modified - signupHandler and signinHandler methods to the AuthenticationResource class

--- src/main/java/com/davr7/springauth/resources/AuthenticationResource.java
+++ src/main/java/com/davr7/springauth/resources/AuthenticationResource.java
@@ -5,24 +5,25 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.davr7.springauth.domain.User;
 import com.davr7.springauth.dtos.CreateUserDTO;
+import com.davr7.springauth.dtos.LoginUserDTO;
+import com.davr7.springauth.dtos.RecoveryJwtTokenDTO;
 import com.davr7.springauth.services.UserService;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("auth")
 public class AuthenticationResource {
-	
 	@Autowired
 	UserService service;
 	
 	@PostMapping("/signup")
-	public ResponseEntity<User> signup(CreateUserDTO obj) {
-
+	public ResponseEntity<User> signupHandler(@RequestBody CreateUserDTO obj) {
 		User user = service.createUser(obj);
 
 		URI location = ServletUriComponentsBuilder
@@ -32,5 +33,11 @@ public class AuthenticationResource {
 				.toUri();
 		
 		return ResponseEntity.created(location).body(user);
+	}
+	
+	@PostMapping("/signin")
+	public ResponseEntity<RecoveryJwtTokenDTO> signinHandler(@RequestBody LoginUserDTO obj) {
+		String token = service.authenticaticateUser(obj);
+		return ResponseEntity.ok().body(new RecoveryJwtTokenDTO(token));
 	}
 }
