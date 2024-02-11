48f1037847a288209d1a9afc593bd64df378cd18
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-11 10:26:25 -0300

    modified - atrribute roles to the List<UserRole> type

--- src/main/java/com/davr7/springauth/dtos/CreateUserDTO.java
+++ src/main/java/com/davr7/springauth/dtos/CreateUserDTO.java
@@ -1,6 +1,8 @@
 package com.davr7.springauth.dtos;
 
+import java.util.List;
+
 import com.davr7.springauth.domain.UserRole;
 
-public record CreateUserDTO(String fullname, String email, String username, String password, UserRole role) {
+public record CreateUserDTO(String fullname, String email, String username, String password, List<UserRole> roles) {
 }
